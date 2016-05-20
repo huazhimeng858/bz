2c95a5e6fe8b42d5f72417bf63e7fb508f90f961
lssemail <lssemail@126.com>
2016-05-20 18:23:57 +0800

    页面重构222

--- js/spa.shell1.js
+++ js/spa.shell1.js
@@ -1,15 +1,13 @@
 /**
- * Created by Administrator on 2016-05-19.
+ * Created by Administrator on 2016/5/20.
  */
 spa.shell = (function () {
-   ////需要的参数
+    ////需要的参数
     var
         configMap = {
             login_disabled:false,
-            user_name_info_warning:false,
             user_idcard_info_warning:false,
             wrong_info_put:true,
-            name_warning_text:'姓名不合法或不为空',
             id_card_warning_text:'身份证不合法或不为空',
             user_phone_have:false,
             again_submit:true,
@@ -22,7 +20,7 @@ spa.shell = (function () {
             $container        : null
 
         },
-        //**变量**//
+    //**变量**//
         jqueryMap = {},
 
         setJqueryMap, initModule;
@@ -35,14 +33,15 @@ spa.shell = (function () {
             $user_testcode:$('.user-testcode'),
             $user_getcode:$('.get-testcode'),
             $user_login:$('.user-login'),
-            $user_name:$('.user-name'),
             $user_id_card:$('.user-id-card'),
             $name_warning_info:$('.name-warning-info'),
             $idcard_warning_info:$('.idcard-warning-info'),
-            $name:$('.user-name').val(),
+            $phone_warning_info:$('.phone-warning-info'),
             $phone:$('.user-phone').val(),
             $idcard:$('.user-id-card').val(),
-            $user_code:$('.user-code')
+            $user_sure:$('.user-sure'),
+            $user_code:$('.user-code'),
+            $time_clock:$('.time-clock')
         };
     };
     ///**初始化页面数据 输入框信息待清空***////
@@ -50,54 +49,20 @@ spa.shell = (function () {
         if(!configMap.user_phone_have){
             jqueryMap.$user_getcode.attr("disabled","disabled");
         };
-        if(jqueryMap.$name==""){
-            return configMap.user_name_info_warning=false;
-        };
         if(jqueryMap.$idcard==""){
+            jqueryMap.$user_login.css({'display':'none'});
+            jqueryMap.$user_sure.css({'display':'none'});
             return configMap.user_idcard_info_warning=false;
         };
-        if(configMap.user_name_info_warning||configMap.user_idcard_info_warning){
+        if(configMap.user_idcard_info_warning){
             return configMap.login_disabled=false;
         };
-        //jqueryMap.$user_login.css({'display':'none'});
-        //configMap.wait_time=59;
-        ////jqueryMap.$user_testcode.val("输入验证码");
-        ////jqueryMap.$user_getcode.val("获取验证码");
-        //jqueryMap.$user_getcode.attr("disabled","disabled");
-
-
     };
     ////****第二个button的提交****///////
     function onSumitToggle(){
-        //jqueryMap.$user_login.attr("disabled","disabled");
-        if(configMap.user_name_info_warning&&configMap.user_idcard_info_warning&&configMap.again_submit){
-            ////***可以提交，禁止多次提交***////
-            console.log(3);
-            console.log(configMap.again_submit);
-            //jqueryMap.$user_login.removeAttr("disabled");
-            configMap.again_submit=false;
-            return true;
-
-        }else if(configMap.user_name_info_warning&&configMap.user_idcard_info_warning&&!configMap.again_submit){
-            console.log(10);
-            onDisable();
-        }else{
-            ///**禁止提交**///
-                return false;
-            //jqueryMap.$user_login.attr("disabled","disabled");
-        }
-
+        jqueryMap.$user_login.attr("disabled","disabled");
     };
     ////****禁止重复提交，待改进/////
-    function onDisable(){
-        jqueryMap.$user_login.submit(function(){
-            jqueryMap.$user_login.attr("disabled","disabled");
-        });
-        //if(configMap.user_name_info_warning&&configMap.user_idcard_info_warning&&!configMap.again_submit){
-        //
-        //}
-        //alert('禁止重复提交');
-    };
     ///////**获取验证码并操作第一个表格提交***////////
     function onGettestcode(){
         jqueryMap.$user_getcode.attr("disabled","disabled");
@@ -105,27 +70,30 @@ spa.shell = (function () {
             countDown(configMap.wait_time--,configMap.wait_speed);
         },configMap.wait_speed);
         function countDown(seconds,speed){
-            var txt = "00 : " + ((seconds < 10) ? "0" + seconds : seconds);
-            jqueryMap.$user_testcode.val(txt);
-            jqueryMap.$user_getcode.text(configMap.get_testcode_text);
             if(seconds == 0){
                 clearTimeout(timeId);
                 jqueryMap.$user_getcode.removeAttr("disabled");
+                jqueryMap.$user_testcode.val("输入验证码");
             };
-        }
-        //countDown(configMap.wait_time,configMap.wait_speed);
-    };
-    //function onCountDown(){
-    //
-    //};
-    /////提示信息//////
-    function onNameWarning_info(){
-        if(configMap.user_name_info_warning){
-            jqueryMap.$name_warning_info.css({'display':'none'});
-        }else{
-            jqueryMap.$name_warning_info.text(configMap.name_warning_text);
+            var txt = "00 : " + ((seconds < 10) ? "0" + seconds : seconds);
+            jqueryMap.$time_clock.text(txt);
+            jqueryMap.$user_getcode.text(configMap.get_testcode_text);
+            jqueryMap.$user_testcode.focus();
         };
+
     };
+    //function onGainGetcode(){
+    //    if(jqueryMap.$user_getcode.text()==configMap.get_testcode_text){
+    //        jqueryMap.$user_testcode.val("");
+    //        //clearTimeout(timeId);
+    //        //configMap.wait_time="59";
+    //        //jqueryMap.$user_getcode.removeAttr("disabled");
+    //    }
+    //};
+    function onSurecode(){
+        jqueryMap.$user_sure.attr("disabled","disabled");
+    };
+    /////提示信息//////
     function onIdcardWarning_info(){
         if(configMap.user_idcard_info_warning){
             jqueryMap.$idcard_warning_info.css({'display':'none'});
@@ -134,53 +102,56 @@ spa.shell = (function () {
         }
     };
     function onPhoneValidate(){
-        onInit();
         var val=jqueryMap.$user_phone.val();
         var reg = /^1[3|4|5|7|8]\d{9}$/;
         if(reg.test(val)){
             jqueryMap.$user_getcode.removeAttr("disabled");
+            jqueryMap.$phone_warning_info.css({'display':'none'});
 
         }
         if(!reg.test(val)){
             jqueryMap.$user_getcode.attr("disabled","disabled");
+            jqueryMap.$phone_warning_info.css({'display':''});
         }
+
     };
     //////输入信息验证///////
-    function onNameValidate(){
-        var val=jqueryMap.$user_name.val();
-        var reg = /^[\u4E00-\u9FA5]{2,4}$/;
-        if(reg.test(val)){
-            configMap.user_name_info_warning=true;
-        }
-        if(!reg.test(val)){
-            configMap.user_name_info_warning=false;
-        }
-        onNameWarning_info();
-    };
     function onIdCardValidate(){
         var val=jqueryMap.$user_id_card.val();
         var reg=/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;
         if(reg.test(val)){
             configMap.user_idcard_info_warning=true;
+            jqueryMap.$user_login.css({'display':''});
         }
         if(!reg.test(val)){
             configMap.user_idcard_info_warning=false;
+            jqueryMap.$user_login.css({'display':'none'});
         }
         onIdcardWarning_info();
+    };
+    function onCodeValidate(){
+        var val=jqueryMap.$user_testcode.val();
+        var reg=/\d{6}/;
+        if(reg.test(val)){
+            jqueryMap.$user_sure.css({'display':''});
+        }
+        if(!reg.test(val)){
+            jqueryMap.$user_sure.css({'display':'none'});
+        }
     };
     ///初始化模块跟绑事件
     initModule = function ( $container ) {
         stateMap.$container = $container;
         setJqueryMap();
         onInit();
-        onSumitToggle();
         jqueryMap.$user_phone.on('input propertychange',onPhoneValidate);
-        jqueryMap.$user_name.on('input propertychange',onNameValidate);
         jqueryMap.$user_id_card.on('input propertychange',onIdCardValidate);
-        jqueryMap.$user_login.on('click',onSumitToggle);
-        $("form:first").submit(onGettestcode);
-        //jqueryMap.$user_getcode.on('click',onGettestcode);
-
+        $("form:first").submit(onSumitToggle);
+        $("form:eq(1)").submit(onGettestcode);
+        jqueryMap.$user_testcode.on('input propertychange',onCodeValidate);
+        $("form:last").submit(onSurecode);
+        //jqueryMap.$user_testcode.focus(onGainGetcode);
+        //jqueryMap.$user_testcode.blur(onGainGetcode);
     };
     return { initModule : initModule };
 
